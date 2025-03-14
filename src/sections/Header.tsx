560352a72b1baa095de8970ac7fd7f0aa6c7a6f5
tsegaye27 <ttalegn29@gmail.com>
2025-03-14 23:08:26 +0300

    feat(header): use next/link for navigation

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,22 +1,23 @@
+import Link from "next/link";
 export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-20">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/5  backdrop-blur">
-        <a href="/" className="nav-item">
+        <Link href="/" className="nav-item">
           Home
-        </a>
-        <a href="/" className="nav-item">
+        </Link>
+        <Link href="/" className="nav-item">
           Experience
-        </a>
-        <a href="/" className="nav-item">
-          Projects
-        </a>
-        <a
+        </Link>
+        <Link href="/" className="nav-item">
+          About
+        </Link>
+        <Link
           href="/"
           className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
         >
           Contact
-        </a>
+        </Link>
       </nav>
     </div>
   );
