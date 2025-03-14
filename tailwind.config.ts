f5b1dee89bb785f7f60b7277e0cf275495e49351
tsegaye27 <ttalegn29@gmail.com>
2025-03-14 23:12:25 +0300

    feat: Add 'move-left' animation for UI elements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,8 +25,22 @@ const config: Config = {
         sans: "var(--font-sans)",
         serif: "var(--font-serif)",
       },
+      animation: {
+        "move-left": "move-left 30s linear infinite",
+      },
+      keyframes: {
+        "move-left": {
+          "0%": {
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            transform: "translateX(-50%)",
+          },
+        },
+      },
     },
   },
+
   plugins: [],
 };
 export default config;
