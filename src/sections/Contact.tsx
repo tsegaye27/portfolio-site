de2e88391f47eb1158d737520d0ca052f50a74e6
tsegaye27 <ttalegn29@gmail.com>
2025-03-14 23:12:14 +0300

    feat: Implement contact section with call to action

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,29 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+export const ContactSection = () => {
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container ">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left">
+          <div className="flex flex-col gap-8 md:gap-16 items-center md:flex-row">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let's create something amazing together
+              </h2>
+              <p className="text-sm mt-2 md:text-base">
+                Ready to bring your next project to life? Let's connect and
+                discuss I can help you achieve your goals.
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl w-max gap-2 border border-gray-900">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
