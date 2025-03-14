a6913588d969a9290e31ef2e7c9ccda415d84d5b
tsegaye27 <ttalegn29@gmail.com>
2025-03-14 23:08:12 +0300

    feat(experience): implement experience section with data display

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,6 +1,11 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import Link from "next/link";
+import Image from "next/image";
+import { Card } from "@/components/Card";
 
 const professionalExperience = [
   {
@@ -42,5 +47,77 @@ const professionalExperience = [
 ];
 
 export const ExperienceSection = () => {
-  return <div>Experience Section</div>;
+  return (
+    <div className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent text-center">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">
+          Professional Experience
+        </h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg lg:text-xl max-w-md mx-auto">
+          I have worked on internship based roles and freelance projects,
+          contributing to real-world applications
+        </p>
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
+          {professionalExperience.map((experience, index) => (
+            <Card
+              key={index}
+              className="px-8 pt-8 md:pt-12 md:px-10 pb-0 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${index * 40}px)`,
+              }}
+            >
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span>{experience.company}</span>
+                    <span>&bull;</span>
+                    <span>{experience.year}</span>
+                  </div>
+                  <div>
+                    <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">
+                      {experience.title}
+                    </h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <ul className="flex flex-col gap-4 mt-4">
+                      {experience.results.map((result, index) => (
+                        <li
+                          key={index}
+                          className="flex gap-2 md:text-base text-sm text-white/50"
+                        >
+                          <CheckCircleIcon className="size-5 md:size-6" />
+                          <span>{result.title}</span>
+                        </li>
+                      ))}
+                    </ul>
+                    <Link
+                      href={experience.link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <button className="bg-white text-gray-950 h-12 w-full px-6 md:w-auto rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                        <span>Visit Live Site</span>
+                        <ArrowUpRightIcon className="size-4 " />
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                <div className="relative">
+                  <Image
+                    className="mt-8 -mb-4 lg:absolute lg:h-full lg:w-auto lg:max-w-none lg:mt-0"
+                    src={experience.image}
+                    alt={experience.title}
+                  />
+                </div>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
