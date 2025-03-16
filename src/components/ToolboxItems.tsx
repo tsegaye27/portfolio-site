d44388d69d7430b3cda4c5d188803a7d8a210e1a
tsegaye27 <ttalegn29@gmail.com>
2025-03-16 19:18:03 +0300

    feat(toolbox): Render toolbox items multiple times for visual emphasis

--- src/components/ToolboxItems.tsx
+++ src/components/ToolboxItems.tsx
@@ -1,4 +1,5 @@
 import { TechIcon } from "@/components/TechIcon";
+import { Fragment } from "react";
 import { twMerge } from "tailwind-merge";
 export const ToolboxItems = ({
   items,
@@ -25,14 +26,18 @@ export const ToolboxItems = ({
           itemsWrapperClassName,
         )}
       >
-        {items.map((item) => (
-          <div
-            key={item.title}
-            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-          >
-            <TechIcon component={item.iconType} />
-            <span className="font-semibold">{item.title}</span>
-          </div>
+        {[...new Array(2)].fill(0).map((_, index) => (
+          <Fragment key={index}>
+            {items.map((item) => (
+              <div
+                key={item.title}
+                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+              >
+                <TechIcon component={item.iconType} />
+                <span className="font-semibold">{item.title}</span>
+              </div>
+            ))}
+          </Fragment>
         ))}
       </div>
     </div>
